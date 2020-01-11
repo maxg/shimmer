da2760f9d0e2489d2134da88a81a57e668239490
Max Goldman <maxg@mit.edu>
2020-01-11 12:28:06 -0500

    Install `awscli` with Yum, installing with pip breaks Certbot

--- setup/setup.sh
+++ setup/setup.sh
@@ -12,10 +12,7 @@ rpm --query nodesource-release-el7-1.noarch || rpm --install --nosignature https
 # Yum Packages
 yum -y update
 yum -y install epel-release centos-release-scl yum-cron zip unzip gcc-c++ make git vim
-yum -y install firewalld httpd mod_ssl certbot python2-certbot-apache shibboleth.x86_64 nodejs python2-pip jq
-
-# AWS CLI
-pip install awscli --upgrade
+yum -y install firewalld httpd mod_ssl certbot python2-certbot-apache shibboleth.x86_64 nodejs awscli jq
 
 # Apache config
 cp httpd/shimmer.conf /etc/httpd/conf.d/
