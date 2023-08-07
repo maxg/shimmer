0fa7c81b5e93e24b0ffbe6868e220c4214a89919
Max Goldman <maxg@mit.edu>
2023-08-07 00:21:12 -0400

    Remove delay that no longer works around a race condition

--- setup/terraform.tf
+++ setup/terraform.tf
@@ -236,8 +236,6 @@ resource "aws_iam_instance_profile" "web" {
 resource "aws_eip_association" "web_address" {
   instance_id = aws_instance.web.id
   allocation_id = aws_eip.web.id
-  # with create_before_destroy, re-association completes before EIP points to new instance
-  provisioner "local-exec" { command = "sleep 9" }
   lifecycle { create_before_destroy = true }
 }
 
